e4f38b3b490b68029e5970263cf694b7f5ee3ee7
Camilo Caceres <camiloac97@gmail.com>
2024-07-12 15:10:37 -0500

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="max-w-lg m-4 h-72 border mx-auto flex flex-col items-center">
+<h1 class="my-6">bulls and cows</h1>
+
+<UButton>
+    Play now
+</UButton>
+<div class="flex">
+    <UButton variant="outline">Sign Up</UButton>
+    <UButton>Login</UButton>
+</div>
+
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
